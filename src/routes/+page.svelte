5b5d8e267587ef6dbae32b3c6ce53a18ecb3393a
crazyvillage <mag@crazyvillage.net>
2024-08-07 15:43:24 -0700

    feat: use flowbite components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,21 @@
-<script lang="ts">
-	import Employee from '$lib/Employee.svelte';
-	import AddEmployee from '$lib/AddEmployee.svelte';
+<div class="bg-white">
+	<header class="absolute inset-x-0 top-0 z-50">
+		<nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+			<div class="hidden lg:flex lg:gap-x-12">
+				<a href="/stores" class="text-sm font-semibold leading-6 text-gray-900">Stores</a>
+				<a href="/prisma" class="text-sm font-semibold leading-6 text-gray-900">Prisma</a>
+			</div>
+		</nav>
+	</header>
 
-	export let data;
-</script>
-
-<div class="card w-45 mx-auto mt-75 pb-5">
-	<table>
-		<thead>
-		<tr>
-			<th>Name</th>
-			<th>Position</th>
-			<th>Salary</th>
-			<th>Action</th>
-		</tr>
-		</thead>
-		<tbody>
-		{#each data.employees as employee}
-			<tr>
-				<Employee
-					{...employee}
-				/>
-			</tr>
-		{/each}
-		</tbody>
-	</table>
-	<form method="POST" action="?/create">
-		<label>
-			Name
-			<input name="name" required>
-		</label>
-		<label>
-			Position
-			<input name="position" required>
-		</label>
-		<label>
-			Salary
-			<input name="salary" type="number" required min="0">
-		</label>
-		<button>Add</button>
-	</form>
+	<div class="relative isolate px-6 pt-14 lg:px-8">
+		<div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+			<div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+		</div>
+		<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+			<div class="text-center">
+				<h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">SvelteKit Demo</h1>
+			</div>
+		</div>
+	</div>
 </div>
-
-<style lang="postcss">
-    :global(html) {
-        background-color: theme(colors.gray.100);
-    }
-</style>
